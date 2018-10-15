decaffb8c0e3948af5d2ec4c5209abffabaa7ae9
jasehackman <jasehackman@gmail.com>
2018-10-15 11:16:07 -0500

    created second js file to store answers

--- newdata.js
+++ newdata.js
@@ -4408,45 +4408,45 @@ const githubData = [
 ]
 
 
-// question 1
-let test = 0
-let commit = 0
+// // question 1
+// let test = 0
+// let commit = 0
 
-for(let i = 0; i < githubData.length; i++){
-  if('payload' in githubData[i]) {
-    for(let pay in githubData[i].payload) {
-      // console.log(pay);
-      if(pay ==='commits') {
-        for(let j = 0; j < githubData[i].payload.commits.length; j++) {
-          commit++;
-        }
-      }
-    }
-  }
-}
-
-
-
-console.log('commit', commit);
-console.log('test', test);
+// for(let i = 0; i < githubData.length; i++){
+//   if('payload' in githubData[i]) {
+//     for(let pay in githubData[i].payload) {
+//       // console.log(pay);
+//       if(pay ==='commits') {
+//         for(let j = 0; j < githubData[i].payload.commits.length; j++) {
+//           commit++;
+//         }
+//       }
+//     }
+//   }
+// }
 
 
-let test2 = 0
-for(let i = 0; i < githubData.length; i++){
-  if('payload' in githubData[i]) {
-    for(let pay in githubData[i].payload) {
-      if(pay ==='pull_request') {
-        for(let pull in githubData[i].payload.pull_request) {
-          if(pull === "commits") {
-            commit += githubData[i].payload.pull_request.commits;
-            test2 += githubData[i].payload.pull_request.commits;
-          }
-        }
-      }
-    }
-  }
-}
 
-console.log("test2", test2);
-console.log('commit', commit);
-// .hasOwnProperty()
+// console.log('commit', commit);
+// console.log('test', test);
+
+
+// let test2 = 0
+// for(let i = 0; i < githubData.length; i++){
+//   if('payload' in githubData[i]) {
+//     for(let pay in githubData[i].payload) {
+//       if(pay ==='pull_request') {
+//         for(let pull in githubData[i].payload.pull_request) {
+//           if(pull === "commits") {
+//             commit += githubData[i].payload.pull_request.commits;
+//             test2 += githubData[i].payload.pull_request.commits;
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// console.log("test2", test2);
+// console.log('commit', commit);
+// // .hasOwnProperty()
