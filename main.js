5bc6779951b941d10df76d04d0217009b82cb617
jasehackman <jasehackman@gmail.com>
2018-10-15 12:00:21 -0500

    completed question 2

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// How many total commits were made in all of Steve's events?
+// How many of each event type are there? (PullRequestEvent, PushEvent, etc)
+// List all Github users who submitted a pull request that was approved by Steve.
+// List all repositories on which Steve had an event, and show how many events were on each one.
+// Which event had the most number of commits?
+// Which programming langugages were affected by Steve's events?
+// What programming language was the most affected by Steve's events?
 
-// question 1
+
+
+
+// question 1: How many total commits were made in all of Steve's events?
 let test = 0
 let commit = 0
 
@@ -41,3 +51,63 @@ for(let i = 0; i < githubData.length; i++){
 console.log("test2", test2);
 console.log('commit', commit);
 // .hasOwnProperty()
+
+
+// Question 2: How many of each event type are there? (PullRequestEvent, PushEvent, etc)
+
+// My solution
+let type = []
+
+for(let i = 0; i < githubData.length; i++){
+  type.push(githubData[i].type);
+}
+
+console.log(type);
+
+let countobj = {
+  pullRequest: 0,
+  pushEvent: 0,
+  deleteEvent: 0,
+  issueCommentEvent: 0,
+  createEvent: 0,
+}
+
+for(let i = 0; i < githubData.length; i++){
+  if (githubData[i].type === "PushEvent") {
+    countobj.pushEvent++
+  } else if (githubData[i].type === "PullRequestEvent") {
+    countobj.pullRequest++
+  }  else if (githubData[i].type === "DeleteEvent") {
+    countobj.deleteEvent++
+  } else if (githubData[i].type === "IssueCommentEvent") {
+    countobj.issueCommentEvent++
+  }  else if (githubData[i].type === "CreateEvent") {
+    countobj.createEvent++
+  }
+}
+
+console.log('countobj', countobj);
+
+
+// David's Solution
+
+let eventTypes = [];
+let eventcounts = {
+  PushEvent: 0,
+  PullRequestEvent: 0,
+  DeleteEvent: 0,
+  IssueCommentEvent: 0,
+  CreateEvent: 0, 
+};
+
+for(let i=0; i<githubData.length;i++) {
+  // get unique events
+  if (!eventTypes.includes(githubData[i].type)) {
+      eventTypes.push(githubData[i].type)
+  }
+  // Access each key adds one to the value of that key
+  eventcounts[githubData[i].type] += 1
+}
+
+console.log(eventTypes);
+console.log(eventcounts);
